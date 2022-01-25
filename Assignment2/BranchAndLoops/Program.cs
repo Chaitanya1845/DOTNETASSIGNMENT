d32c25398de153ed0829d2af9b86f8b74c8961a2
Chaitanya Govind Nagdev <nagdev.chaitanya@gmail.com>
2022-01-25 09:19:55 +0530

    DotNet Assignment

---
+++ Assignment2/BranchAndLoops/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b, ans;
+
+Console.WriteLine("\nExample of If Statement");
+a = 5;
+b = 6;
+ans = a + b;
+Console.WriteLine($"a={a},b={b},ans={a + b}");
+if (ans > 10)
+    Console.WriteLine("The ans is greater than 10.");
+
+
+Console.WriteLine("\nExample of if else statement");
+a = 5;
+b = 3;
+ans = a + b;
+Console.WriteLine($"a={a},b={b},ans={a + b}");
+if (ans > 10)
+    Console.WriteLine("The ans is greater than 10.");
+else
+    Console.WriteLine("The ans is less than 10.");
+
+Console.WriteLine("\nLOOPS");
+Console.WriteLine("\nExample of While Loop");
+int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine($"Counter using while loop is {counter}");
+    counter++;
+}
+
+
+Console.WriteLine("\nExample of DO While Loop");
+counter = 0;
+do
+{
+    Console.WriteLine($"Counter using do while loop is {counter}");
+    counter++;
+} while (counter < 10);
+
+Console.WriteLine("\nExample of For Loop");
+for (int i = 0; i < 10; i++)
+{
+
+    Console.WriteLine($"Counter  using for loop is {i}");
+}
+
+Console.WriteLine("\n Making Matrix Using Nested Loops");
+for (int row = 1; row < 4; row++)
+{
+    for (char column = 'a'; column < 'd'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
+
+Console.WriteLine("\nProgram to find sum of integer 1 to 20 which are divisible by 3");
+int sum = 0;
+for (int i = 1; i <= 20; i++)
+{
+    if (i % 3 == 0)
+        sum += i;
+}
+Console.WriteLine($"The Sum is {sum}");
---
+++ Assignment2/Numbers/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*Code 
+Explore Integer Math
+*/
+
+Console.WriteLine("Explore Integer Math");
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+c = a - b;
+Console.WriteLine(c);
+c = a * b;
+Console.WriteLine(c);
+c = a / b;
+Console.WriteLine(c);
+
+/*Explore order of operation*/
+
+Console.WriteLine("\nExplore order of operation");
+a = 5;
+b = 4;
+c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+
+d = (a + b) * c;
+Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+d = (a + b) / c;
+Console.WriteLine(d);
+
+/*Explore Integer Precision and limits */
+Console.WriteLine("\nExplore Integer Precision and limits");
+a = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+/*Range of int data type*/
+int maxInt = int.MaxValue;
+int minInt = int.MinValue;
+Console.WriteLine($"The range of integers is {minInt} to {maxInt}");
+
+/*Explame of Overflow*/
+int what = maxInt + 3;
+Console.WriteLine($"An example of overflow: {what}");
+
+
+/*Working  with Double Type*/
+Console.WriteLine("\nExample of double Data Type");
+double x = 19;
+double y = 23;
+double z = 8;
+double w = (x + y) / z;
+Console.WriteLine($"w={w}");
+
+double maxDouble = double.MaxValue;
+double minDouble = double.MinValue;
+Console.WriteLine($"The range of double is {minDouble} to {maxDouble}");
+
+/*Working with Decimal Type*/
+Console.WriteLine("\nWorking with Decimal DataType");
+decimal minDecimal = decimal.MinValue;
+decimal maxDecimal = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {minDecimal} to {maxDecimal}");
+
+Console.WriteLine("\nComparing precision of data type DECIMAL AND DOUBLE");
+x = 1.0;
+y = 3.0;
+Console.WriteLine($"Ans of 1/3 for double data type{x / y}");
+decimal i = 1.0M;
+decimal j = 3.0M;
+Console.WriteLine($"Ans of 1/3 for decimal data type{i / j}");
+
+
+Console.WriteLine("\nProgram For Calculating Area of Circle");
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine($"Area of Circle With Radius= {radius} is {area}\n");

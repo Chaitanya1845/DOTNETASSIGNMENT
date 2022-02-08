cc3cb390c1b9f1f096eb43c057177333bc6dc94d
Chaitanya Govind Nagdev <nagdev.chaitanya@gmail.com>
2022-02-08 15:36:10 +0530

    Assignment4 Added

---
+++ Assignment4/Employee/Program.cs
@@ -0,0 +1,129 @@
+﻿
+using System;
+using System.Reflection;
+
+
+public class EmployeeTest
+{
+    static void Main()
+    {
+
+        Employee E1 = new Employee("Raj", "Jhadav", 55000);
+        Employee E2 = new Employee("Mihir", "Agarwal", 62000);
+
+        Console.WriteLine("\nBefore Raise");
+        Console.WriteLine(E1);
+        Console.WriteLine(E2);
+        E1.getRaise(10);
+        E2.getRaise(10);
+        Console.WriteLine("\nAfter Raise");
+        Console.WriteLine(E1);
+        Console.WriteLine(E2);
+
+        Console.WriteLine("\n------------Permanent Employees--------");
+
+        PermanentEmployee PE1 = new PermanentEmployee("Smit", "Shah", 40000, 15000, 1000, 2000, "01-2-2022", "01-2-2023");
+        PermanentEmployee PE2 = new PermanentEmployee("Bhavin", "Sheth", 50000, 13000, 1500, 1000, "01-5-2022", "01-1-2023");
+
+        Console.WriteLine("\nBefore Raise");
+        Console.WriteLine(PE1);
+        Console.WriteLine(PE2);
+        PE1.getRaise(10);
+        PE2.getRaise(10);
+        Console.WriteLine("\nAfter Raise");
+        Console.WriteLine(PE1);
+        Console.WriteLine(PE2);
+
+
+    }
+}
+public class Employee
+{
+    protected string firstName;
+    protected string lastName;
+    protected double mSalary;
+
+    public Employee(string first, string last, double salary)
+    {
+        firstName = first;
+        lastName = last;
+        mSalary = salary;
+    }
+
+    public string FirstName
+    {
+        get => firstName;
+        set => firstName = value;
+    }
+
+    public string LastName
+    {
+        get => LastName;
+        set => LastName = value;
+    }
+    public double Salary
+    {
+        get => mSalary;
+        set => mSalary = value < 0 ? 0.0 : value;
+    }
+    public virtual void getRaise(double percentage)
+    {
+        mSalary += (percentage / 100) * mSalary;
+    }
+    public override string ToString()
+    {
+        return "Employee: " + firstName + " " + lastName + " Montly Salary: " + mSalary;
+    }
+}
+public class PermanentEmployee : Employee
+{
+    private string joiningDate;
+    private string retirementDate;
+    private double hra;
+    private double da;
+    private double pf;
+    public PermanentEmployee(string first, string last, double salary, double HRA, double DA, double PF, string jDate, string rDate) : base(first, last, salary)
+    {
+        hra = HRA;
+        da = DA;
+        pf = PF;
+        this.joiningDate = jDate;
+        this.retirementDate = rDate;
+    }
+    public double Hra
+    {
+        get => hra;
+    }
+    public double Da
+    {
+        get => da;
+    }
+    public double Pf
+    {
+        get => pf;
+    }
+    public string JoiningDate
+    {
+        get => joiningDate;
+        set => joiningDate = value;
+    }
+    public string RetirementDate
+    {
+        get => retirementDate;
+        set => retirementDate = value;
+    }
+    public override void getRaise(double percentage)
+    {
+        mSalary = mSalary + ((mSalary * percentage) / 100) + hra + da;
+    }
+    public override string ToString()
+    {
+        return "Permanent Employee: " + firstName + " " + lastName + " Monthly Salary: " + mSalary + " Joining Date: " + joiningDate + " Retirement Date: " + retirementDate;
+    }
+}
+
+
+
+
+
+
---
+++ Assignment4/Properties_Indexers/Program.cs
@@ -0,0 +1,139 @@
+﻿
+using System;
+using System.Globalization;
+
+
+namespace Lab4Examples
+{
+    class Examples
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("\nProperties\n");
+            var person = new Person("Chaitanya", "Nagdev");
+            Console.WriteLine($"name:{person.Name}  Time:{DateTime.Now.ToString("HH:mm:ss tt")}");
+
+            var item = new SaleItem("Shoes", 19.95m);
+            Console.WriteLine($"{item.Name}: sells for {item.Price}");
+
+            var item2 = new SaleItem2 { Name = "Purse", Price = 20.95m };
+            Console.WriteLine($"{item2.Name}: sells for {item2.Price}");
+
+            Console.WriteLine("\nIndexers\n");
+
+            Console.WriteLine("Example 1\n");
+            var tempRecord = new TempRecord();
+
+
+            tempRecord[3] = 58.3F;
+            tempRecord[4] = 60.1F;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Element #{i} = {tempRecord[i]}");
+            }
+            Console.WriteLine("\nExample2\n");
+
+            var week = new DayCollection();
+            Console.WriteLine($"Index of Friday is {week["Fri"]}");
+
+            try
+            {
+                Console.WriteLine(week["Made-up day"]);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Not supported input: {e.Message}");
+            }
+        }
+
+    }
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        public Person(string first, string last)
+        {
+            firstName = first;
+            lastName = last;
+        }
+
+        public string Name => $"{firstName} {lastName}";
+
+    }
+    public class SaleItem
+    {
+        string _name;
+        decimal _cost;
+
+        public SaleItem(string name, decimal cost)
+        {
+            _name = name;
+            _cost = cost;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public decimal Price
+        {
+            get => _cost;
+            set => _cost = value;
+        }
+    }
+    public class SaleItem2
+    {
+        public string Name
+        { get; set; }
+
+        public decimal Price
+        { get; set; }
+    }
+
+    //INDEXERS 
+    public class TempRecord
+    {
+        float[] temps = new float[5]
+        {
+        56.2F, 56.7F, 56.5F, 56.9F, 58.8F
+        };
+
+
+        public int Length => temps.Length;
+
+        public float this[int index]
+        {
+            get => temps[index];
+            set => temps[index] = value;
+        }
+    }
+    class DayCollection
+    {
+        string[] days = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
+
+        public int this[string day] => FindDayIndex(day);
+
+        private int FindDayIndex(string day)
+        {
+            for (int j = 0; j < days.Length; j++)
+            {
+                if (days[j] == day)
+                {
+                    return j;
+                }
+            }
+
+            throw new ArgumentOutOfRangeException(
+                nameof(day),
+                $"Day {day} is not supported.\nDay input must be in the form \"Sun\", \"Mon\", etc");
+        }
+    }
+
+}
